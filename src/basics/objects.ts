a4899abe9ef0c49a70af4e1301b98772afcfc82b
TonmoyZohani <tonmoyzohani@gmail.com>
2025-11-09 19:12:02 +0600

    rest added

--- src/basics/objects.ts
+++ src/basics/objects.ts
@@ -5,22 +5,48 @@
 // };
 // console.log(cricketer);
 
-type Player = {
-  type: "Player";
+// type Player = {
+//   type: "Player";
+//   name: string;
+//   age: number;
+// };
+
+// const player: Player = {
+//   type: "Player",
+//   name: "Tonmoy",
+//   age: 25,
+// };
+
+// console.log(player);
+
+// const playerDetails = (cricketer: Player, heighest: number): string => {
+//   return `${cricketer.name} is ${cricketer.age} years old.His best is ${heighest} meters.`;
+// };
+
+// console.log(playerDetails(player, 123));
+
+const sendInvite = (...friends: string[]) => {
+  console.log(friends);
+  friends.map((friend: string): void => {
+    console.log(friend);
+  });
+};
+
+sendInvite("Tonmoy", "Karim", "Zabir", "Hamid");
+
+// const sendInvite = (...friends: string[]): void => {
+//   console.log(friends);
+// };
+
+// sendInvite("Tonmoy", "Karim", "Zabir", "Hamid");
+
+// const sendInvite = (...friends: string[]): void => {
+//   console.log(friends);
+// };
+
+// sendInvite("Tonmoy", "Karim", "Zabir", "Hamid");
+
+type Person = {
   name: string;
   age: number;
 };
-
-const player: Player = {
-  type: "Player",
-  name: "Tonmoy",
-  age: 25,
-};
-
-console.log(player);
-
-const playerDetails = (cricketer: Player, heighest: number): string => {
-  return `${cricketer.name} is ${cricketer.age} years old.His best is ${heighest} meters.`;
-};
-
-console.log(playerDetails(player, 123));
