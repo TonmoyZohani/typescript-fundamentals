f625e6f3762d23bbcaecd4e58880b1272c7fe00a
TonmoyZohani <tonmoyzohani@gmail.com>
2025-11-18 01:18:32 +0600

    problem 6 fixed

--- src/solution.ts
+++ src/solution.ts
@@ -108,3 +108,31 @@ const users = [
 ];
 
 console.log(filterActiveUsers(users));
+
+/*============================================================================
+ Problem - 6                         
+============================================================================*/
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+function printBookDetails(book: Book): void {
+  const availability = book.isAvailable ? "Yes" : "No";
+  console.log(
+    `Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${availability}`
+  );
+}
+
+// Sample Input
+const myBook: Book = {
+  title: 'The Great Gatsby',
+  author: 'F. Scott Fitzgerald',
+  publishedYear: 1925,
+  isAvailable: true,
+};
+
+printBookDetails(myBook);
